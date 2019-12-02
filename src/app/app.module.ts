2a58212dc33289151e1d23c649f602c48a2e58b3
Jose Hernandez <jose2889@gmail.com>
2019-12-02 11:47:02 -0500

    proyecto para mapas y direcciones en google mps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCFLJrsk7B0nncccNq6hKVE1me5-ZSQj9U',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
